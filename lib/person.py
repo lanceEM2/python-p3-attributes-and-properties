834294701d2039ae629f0b05eb64db7f247d766c
Dylan <dylanlancekemboi@gmail.com>
2023-12-13 17:18:53 +0300

    Completed Lab

--- lib/person.py
+++ lib/person.py
@@ -17,3 +17,29 @@ APPROVED_JOBS = [
 
 class Person:
     pass
+    def __init__(self, name, job):
+        self.name = name
+        self.job = job
+        self._name = None
+        self._job = None
+
+    def name(self):
+        return self._name
+
+    def name(self, value):
+        if type(value) is not str or not (1 <= len(value) <= 25):
+            print("Name must be a string under 25 characters.")
+        else:
+            self._name = value.title()      # title case.
+
+    def job(self):
+        return self._job
+
+    def job(self, value):
+        if value not in APPROVED_JOBS:
+            print("Job must be in the list of approved jobs.")
+        else:
+            self._job = value
+
+person_instance = Person(name="john doe", job="Admin")
+print(person_instance.name)
